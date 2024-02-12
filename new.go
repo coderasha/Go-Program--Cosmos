b5668e365c2f0009a97b2d5cd19cd2529f3046e7
coderasha <ashadipta@gmail.com>
2024-02-12 13:27:42 +0530

    new change

--- new.go
+++ new.go
@@ -3,5 +3,5 @@ import "fmt"
 func main() {
     a, b := 2, 3
     c := float64(a + b)
-    fmt.Printf("%v + %v = %f = %v, stored as %T", a, b, c, c, c)
+    fmt.Printf("%v + %v = %f = %v, stored as %T", a, b, c, c,c )
 }
